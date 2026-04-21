a78982234fb957deea37cc35d99fc19911fbf580
wakalawacho-png <wakalawacho@gmail.com>
2026-04-21 15:55:21 +0200

    ex1 variables especials i condicionals

--- ASO-main/Exercicis/02.Variables especials/server.sh
+++ ASO-main/Exercicis/02.Variables especials/server.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-S="$1"
-SERVER="$2"
-U="$3"
-USUARI="$4"
-P="$5"
-CONTRA="$6"
-
-if [[ $S -eq -s ]]; then
-	if [[ $U -eq -u ]]; then
-		if [[ $P -eq -p ]]; then
-			echo "conectat"
-		fi
-	fi
-	else
-	echo "Us: ./server.sh -s Server -u NomUsuari [-p Contraseña]"
+if [ "$1" = "-s" ]; then
+	SERVER="$2"
 fi
+if [ "$3" = "-u" ]; then
+	USUARI="$4"
+fi
+if [ "$5" = "-p" ]; then
+	CONTRASENYA="$6"
+fi
+if [ -z "$SERVER" ] || [ -z "$USUARI" ] || [ -z "$CONTRASENYA" ]; then
+	echo " us: $0 -s servidor -u nomusuari -p contrasenya]"
+	else
+	echo "conectat"
+fi
+
